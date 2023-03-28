a2f23a3c60a59802ce5eba94082077112ed89240
tylereriksen <saeji.hong@gmail.com>
2023-03-28 11:10:30 -0700

    Comments Added

--- BoardBuilder.h
+++ BoardBuilder.h
@@ -2,21 +2,68 @@
 #define BOARD_BUILDER_H
 #include "BoardGame.h"
 
+/**
+ * builder class is used to build the boardgame
+ * used a builder design pattern in case the default values were wanted
+ * and were not specified to make the board and game
+ */
 class BoardBuilder{
 
     private:
+        /**
+         * private instance variables of the class
+         */
         int inarow;
         int players;
         int rows;
         int cols;
 
     public:
+
+        /**
+         * @brief Construct a new Board Builder object
+         */
         BoardBuilder();
+
+        /**
+         * @brief Destroy the Board Builder object
+         */
         ~BoardBuilder();
+
+        /**
+         * @brief build a board with a particular number of rows
+         * @param r - number of rows
+         * @return BoardBuilder* this boardgame builder
+         */
         BoardBuilder* withRows(int r);
+
+        /**
+         * @brief build a board with a particular number of columns
+         * @param c - number of columns
+         * @return BoardBuilder* this boardgame builder
+         */
         BoardBuilder* withCols(int c);
+
+        /**
+         * @brief build a game with the condition that winning involves
+         * a certain number of tokens in a row
+         * @param i - the number of tokens in a row to win
+         * @return BoardBuilder* this boardgame builder
+         */
         BoardBuilder* withInarow(int i);
+
+        /**
+         * @brief build a game with a particular number of players to play
+         * @param p - number of players needed to play the game
+         * @return BoardBuilder* this boardgame builder
+         */
         BoardBuilder* withPlayers(int p);
+
+        /**
+         * @brief build the boardgame with the current state of instance
+         * variables
+         * @return BoardGame* BoardGame with these current parameters 
+         */
         BoardGame* build();
 };
 
--- BoardGame.h
+++ BoardGame.h
@@ -5,9 +5,17 @@
 #include <vector>
 using namespace std;
 
+/**
+ * class of the boardgame and its functionalities
+ * overall this class defines the board and the overall premise
+ * of the game to be played
+ */
 class BoardGame {
 
     private:
+        /**
+         * private instance variables of the class
+         */
         int rows;
         int cols;
         int inarow;
@@ -15,22 +23,107 @@ class BoardGame {
         vector< vector<string> > board;
         vector<string> tokens;
         int m;
+
+        /**
+         * @brief Get the Board
+         * @return vector< vector<string> > the board 
+         */
         vector< vector<string> > getBoard();
-        bool checkColFull(int c);
+
+        /**
+         * @brief checks to see if there is any rows in-a-row
+         * @return true if there is
+         * @return false if not
+         */
         bool isRowWin();
+
+        /**
+         * @brief checks to see if there is any cokumn in-a-row
+         * @return true if there is
+         * @return false if not
+         */
         bool isColWin();
+
+        /**
+         * @brief checks to see if there is any top-to-bottom diagonal in-a-row
+         * @return true if there is
+         * @return false if not
+         */
         bool isDiagonalTBWin();
+
+        /**
+         * @brief checks to see if there is any bottom-to-top diagonal in-a-row
+         * @return true if there is
+         * @return false if not
+         */
         bool isDiagonalBTWin();
 
     public:
+        /**
+         * @brief Construct a new Board Game object
+         * @param rows - number of rows the boardgame will have
+         * @param cols - number of cols the boardgame will have
+         * @param inarow - the number of tokens in a row you need to get to win
+         * @param players - the number of players playing in a game
+         */
         BoardGame(int rows, int cols, int inarow, int players);
+
+        /**
+         * @brief Destroy the Board Game object
+         */
         ~BoardGame();
+
+        /**
+         * @brief displays/prints the board
+         */
         void displayBoard();
+
+        /**
+         * @brief puts the player's token in a column
+         * @param c - the column index shown in the display
+         */
         void move(int c);
+
+        /**
+         * @brief checks if a move is valid
+         * @param c - the column index shown in the display
+         * @return true if a token can be placed in that column
+         * @return false if token cannot be placed or if column is invalid
+         */
         bool checkValidMove(int c);
+
+        /**
+         * @brief checks if a column is full or not
+         * @param c - the column that we want to check
+         * @return true if the column is filled
+         * @return false if the column is not filled 
+         */
+        bool checkColFull(int c);
+
+        /**
+         * @brief checks if the entire board is filled
+         * @return true if the board is completely filled
+         * @return false if the board is not completely filled
+         */
         bool isBoardFull();
+
+        /**
+         * @brief outputs the token of the player's turn
+         * @return string token of the current player
+         */
         string currentTurn();
+
+        /**
+         * @brief outputs the token of the previous player's turn
+         * @return string token of the previous player
+         */
         string prevTurn();
+
+        /**
+         * @brief checks if there is a inarow in-a-row amount of the same tokens
+         * @return true if there is a winner
+         * @return false if there is no winner
+         */
         bool isWin();
 };
 
